b3085a8d68c347e2a395deb5b3409cccaa730f4e
davidhigginbotham <dhigginbotham@clearent.com>
2017-10-08 06:11:57 -0500

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/Sample.java
@@ -0,0 +1,18 @@
+package com.domain;
+
+public class Sample {
+
+    private String var;
+
+    public Sample(String var) {
+        this.var = var;
+    }
+
+    public String getVar() {
+        return var;
+    }
+
+    public void setVar(String var) {
+        this.var = var;
+    }
+}
